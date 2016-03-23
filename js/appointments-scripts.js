cd698af89c1211c0b08fd4ca1cae6c103213a3ef
Anthony Gold and Kevin Deganos <anthony@>
2016-03-23 14:44:53 -0700

    created site that takes client name, a date, and a start and end time, then reports back the date and times

--- js/appointments-scripts.js
+++ js/appointments-scripts.js
@@ -1,46 +1,18 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $(".jumbotron form").submit(function(event) {
+
+    var nameInput = $("input#name").val();
+    var dateInput = $("input#date").val();
+    var startInput = $("input#start").val();
+    var endInput = $("input#end").val();
+
+    $(".name").text(nameInput);
+    $(".date").text(dateInput);
+    $(".start").text(startInput);
+    $(".end").text(endInput);
+
+    $("#confirmation").show();
+
     event.preventDefault();
-    var number1 = parseInt($("input#add1").val());
-    var number2 = parseInt($("input#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#subtract").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("input#subtract1").val());
-    var number2 = parseInt($("input#subtract2").val());
-    var result = number1 - number2;
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("input#multiply1").val());
-    var number2 = parseInt($("input#multiply2").val());
-    var result = number1 * number2;
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("input#divide1").val());
-    var number2 = parseInt($("input#divide2").val());
-    var result = number1 / number2;
-    $("#output").text(result);
   });
 });
